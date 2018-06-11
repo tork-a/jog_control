20586df3b515333ca88f8000186feb6b54ed56d7
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-06-11 17:39:17 +0900

    Fix the joy_to_jog_frame node to work

--- jog_controller/script/joy_to_jog_frame.py
+++ jog_controller/script/joy_to_jog_frame.py
@@ -17,29 +17,39 @@ from sensor_msgs.msg import Joy
 class joy_to_jog_frame:
 
     def __init__(self):
+        self.enable_button = rospy.get_param('~enable_button', 4)
+        self.angular_button = rospy.get_param('~angular_button', 5)
+        self.axis_linear = rospy.get_param('~axis_linear', {'x': 0, 'y': 1, 'z': 4})
+        self.axis_angular = rospy.get_param('~axis_angular', {'x': 0, 'y': 1, 'z': 4})
+        self.scale_linear = rospy.get_param('~scale_linear', {'x': 0.05, 'y': 0.05, 'z': 0.05})
+        self.scale_angular = rospy.get_param('~scales_angular', {'x': 0.05, 'y': 0.05, 'z': 0.05})
+
         self.pub = rospy.Publisher('jog_frame', JogFrame, queue_size=1)
 
     # Convert to JogFrame and republish
     def callback(self, joy):
 
+        if not joy.buttons[self.enable_button]:
+            return
+
         msg = JogFrame()
 
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = 'base_link'
-        msg.group_name = rospy.get_param('~group_name', 'arm')
+        msg.header.frame_id = rospy.get_param('~frame_id', 'base_link')
+        msg.group_name = rospy.get_param('~group_name', 'manipulator')
+        msg.link_name = rospy.get_param('~link_name', 'tool0')
 
-        # These buttons are binary
-        msg.linear.x = 0.05*(-joy.buttons[4] + joy.buttons[5])
-        # Double buttons
-        msg.linear.y = 0.05*(joy.axes[0])
-        msg.linear.z = 0.05*(joy.axes[1])
-
-        msg.angular.x = 0
-        msg.angular.y = 0
-        # These buttons are binary
-        msg.angular.z = 0
+        if joy.buttons[self.angular_button]:
+            msg.angular_delta.x = self.scale_angular['x']*joy.axes[self.axis_angular['x']]
+            msg.angular_delta.y = self.scale_angular['y']*joy.axes[self.axis_angular['y']]
+            msg.angular_delta.z = self.scale_angular['z']*joy.axes[self.axis_angular['z']]
+        else:
+            # These buttons are binary
+            msg.linear_delta.x = self.scale_linear['x']*joy.axes[self.axis_linear['x']]
+            msg.linear_delta.y = self.scale_linear['y']*joy.axes[self.axis_linear['y']]
+            msg.linear_delta.z = self.scale_linear['z']*joy.axes[self.axis_linear['z']]
+        
         msg.avoid_collisions = True
-
         self.pub.publish(msg)
         
     def republish(self):

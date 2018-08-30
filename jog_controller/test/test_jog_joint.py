3af38846aa5c1eaa7355586819862af2de44626f
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-08-30 12:32:41 +0900

    Add waitForTransform for test code
    - make it optional to specify controller_name (should be remove later)

--- jog_controller/test/test_jog_joint.py
+++ jog_controller/test/test_jog_joint.py
@@ -26,8 +26,8 @@ class TestJogJointNode(unittest.TestCase):
 
     def setUp(self):
         # Get parameters
-        self.controller_name = rospy.get_param('~controller_name', 'arm_controller')
-        self.action_name = rospy.get_param('~action_name', 'follow_joint_trajectory')
+        self.controller_name = rospy.get_param('~controller_name', None)
+        self.action_name = rospy.get_param('~action_name', None)
         self.joint_names = rospy.get_param('~joint_names', [])
         self.home_positions = rospy.get_param('~home_positions', [])
         self.delta = rospy.get_param('~delta', 0.01)
@@ -37,11 +37,13 @@ class TestJogJointNode(unittest.TestCase):
         # Publishers
         self.pub = rospy.Publisher('jog_joint', JogJoint, queue_size=50)
         # Actionlib
-        self.client = actionlib.SimpleActionClient(
-            self.controller_name + '/' + self.action_name, FollowJointTrajectoryAction)
-        self.client.wait_for_server()
-        # Move to home position
-        self.move_to_home()
+        self.client = None
+        if self.controller_name and self.action_name:
+            self.client = actionlib.SimpleActionClient(
+                self.controller_name + '/' + self.action_name, FollowJointTrajectoryAction)
+            self.client.wait_for_server()
+            # Move to home position
+            self.move_to_home()
         
     def move_to_home(self):
         goal = FollowJointTrajectoryGoal()
@@ -53,9 +55,11 @@ class TestJogJointNode(unittest.TestCase):
         point.positions = self.home_positions
         point.time_from_start = rospy.Duration(1.0)
         goal.trajectory.points.append(point)
-        self.client.send_goal(goal)
-        self.assertTrue(self.client.wait_for_result(timeout=rospy.Duration(5.0)))
-        rospy.sleep(1.0)
+
+        if self.client:
+            self.client.send_goal(goal)
+            self.assertTrue(self.client.wait_for_result(timeout=rospy.Duration(5.0)))
+            rospy.sleep(1.0)
         
     def cb_joint_states(self, msg):
         self.joint_state_ = msg

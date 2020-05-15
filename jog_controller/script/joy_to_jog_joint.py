059407e2132fe327c33f5623fd1598d11aee0acc
jpsantos-motofil <jpsantos@motofil.com>
2020-05-15 12:14:02 +0100

    changed joint button config

--- jog_controller/script/joy_to_jog_joint.py
+++ jog_controller/script/joy_to_jog_joint.py
@@ -30,11 +30,11 @@ class joy_to_jog_joint:
 	msg.joint_names = rospy.get_param("controller_joint_names","['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']")
         # These buttons are binary
         msg.deltas = [0]*6
-        msg.deltas[0] = 0.05*(-joy.buttons[4] + joy.buttons[5])
-        msg.deltas[1] = 0.05*(joy.axes[0])
+        msg.deltas[0] = 0.05*(joy.axes[3])
+        msg.deltas[1] = 0.05*(joy.axes[2])
         msg.deltas[2] = 0.05*(joy.axes[1])
-        msg.deltas[3] = 0.05*(joy.buttons[0] - joy.buttons[1])
-        msg.deltas[4] = 0.05*(joy.buttons[2] - joy.buttons[3])
+        msg.deltas[3] = 0.05*(joy.axes[0])
+        msg.deltas[4] = 0.05*(joy.buttons[5] - joy.buttons[4])
         msg.deltas[5] = 0.05*(joy.buttons[7] - joy.buttons[6])
 
         self.pub.publish(msg)
